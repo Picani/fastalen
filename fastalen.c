bd21f72bc2179b0b793e1f51fe549898144db8f4
Picani <picani@laposte.net>
2019-09-15 15:13:24 +0200

    Version 1.4

--- fastalen.c
+++ fastalen.c
@@ -76,7 +76,7 @@ int main(int argc, char *argv[])
     };
 
     char progname[] = "fastalen";
-    char progversion[] = "1.3";
+    char progversion[] = "1.4";
     char kseqversion[] = "19JAN2017 (commit d0b8b55)";
     char argtable3version[] = "3.0.3";
 
